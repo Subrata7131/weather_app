3f019d5b799c7cdfe0bea2eb32547b804fc39107
Rakesh Kundu <159798591+Raxku2@users.noreply.github.com>
2025-09-25 21:24:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from dotenv import load_dotenv
+from os import getenv
+from requests import get
 
 load_dotenv()
-
-from os import getenv
-
-
 WEATHER_API_KEY = getenv("API_KEY")
-
-from requests import get
 
 usr_location = input("Enter Location: ")
 URI = f"http://api.weatherapi.com/v1/current.json?key={WEATHER_API_KEY}&q={usr_location}&aqi=yes"
@@ -16,14 +12,13 @@ if usr_location.lower() == "q":
     exit()
 
 res = get(URI)
-# print(res.json())
+
 apiResData: dict = res.json()
 if res.status_code <= 200 and res.status_code < 400:
     location = apiResData.get("location")
     weather = apiResData.get("current")
 
     print(f"Weather of {location['name']}, {location['region']}")
-
     print(
         f"""--------------------------------------------
     Temp: {weather.get('temp_c'):<5}°C  | Feels: {weather["feelslike_c"]:<5}°C |
@@ -34,3 +29,4 @@ elif res.status_code <= 400:
     print(f"{res.json()}")
 else:
     print("hoyni, pore aso")
+
